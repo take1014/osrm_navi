c4af144ad8f9ba71c03040e357df5cdbe0f0d3fe
take1014 <y.takehara1014@gmail.com>
2024-12-09 00:32:38 +0900

    add --rm option

--- osrm/osrm_run.sh
+++ osrm/osrm_run.sh
@@ -1,5 +1,5 @@
 cd ~/japan
-sudo docker run -t -i -p 5000:5000 -v "${PWD}:/data" osrm-backend-arm64 osrm-routed \
+sudo docker run -t -i -p 5000:5000 --rm -v "${PWD}:/data" osrm-backend-arm64 osrm-routed \
     --algorithm mld \
     --memory=4G \
     --threads=4 \
